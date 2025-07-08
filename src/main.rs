32e1270a16b99abe15b560b30a6b8b75f4737900
lathan1623 <lathan1623@icloud.com>
2025-07-07 20:39:42 -0500

    Handle sub directories

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,34 @@
-use std::{fs, path::Path, error::Error};
+use std::{fs, path::Path};
 
 fn main() -> std::io::Result<()> {
-    let _ = fs::create_dir("./static");
-    build_static_dir("md");
+    let _ = fs::create_dir("public");
+    parse_markdown_files(Path::new("md"), Path::new("public"))?;
     Ok(())
 }
 
-/// read through the md directory, looking for .md files
-/// we want to convert those .md files into html files, so they can eventually be linked to in
-/// index.html
-/// if we find a sub-directory in here. we still want to look through there for .md files too
-/// index.html should not genereate links to those .md files directly. Instead it should create a
-/// link to a page with the name of the directory. that page will then link to those .md files in
-/// the folder
-/// the goal of this function should be to convert all .md files into .html files. and move them to
-/// the static directory.
-fn build_static_dir(dir_path: &str) {
-    let destination = Path::new("./static");
-    let dir = fs::read_dir(dir_path).expect("Cannot find the {dir_path} directory");
-    for entry in dir {
-        let path = entry.unwrap().path();
-        if let Some(extension) = path.extension() {
+fn parse_markdown_files(src: &Path, dest: &Path) -> std::io::Result<()> {
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let path = entry.path();
+        let relative_path = path.strip_prefix("md").unwrap();
+        let target_path = dest.join(relative_path);
+        if path.is_dir() {
+            fs::create_dir_all(&target_path)?;
+            parse_markdown_files(&path, dest)?;
+        } else if let Some(extension) = path.extension() {
             if extension == "md" {
                 let markdown = fs::read_to_string(&path).unwrap();
                 let parser = pulldown_cmark::Parser::new_ext(&markdown, pulldown_cmark::Options::all());
                 let mut body = String::new();
                 pulldown_cmark::html::push_html(&mut body, parser);
-                let mut file_name = std::path::PathBuf::from("./static");
-                file_name.push(&path.file_stem().unwrap());
-                file_name.set_extension("html");
-                let _ = fs::write(file_name, body);
+                let html_target = target_path.with_extension("html");
+                fs::create_dir_all(html_target.parent().unwrap())?;
+                fs::write(html_target, body)?;
+            } else if extension == "html" {
+                fs::create_dir_all(target_path.parent().unwrap())?;
+                fs::copy(&path, &target_path)?;
             }
-            else if extension == "html" {
-                let name = path.file_name().unwrap();
-                //TODO convert md to html, just move html directly
-                let _ = fs::copy(&path, &destination.join(name));
-            }
-        } else {
-            //TODO handle directories here with recursion
-
         }
     }
+    Ok(())
 }
