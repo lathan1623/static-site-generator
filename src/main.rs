636b368defc4936ad864b7a15d653c95871c918e
lathan1623 <lathan1623@icloud.com>
2025-07-09 22:52:34 -0500

    Add file watching and serve website on public directory

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
-use std::{fs, path::Path};
+use std::{error::Error, fs, path::Path, thread, time::Duration};
+
+use axum::Router;
 
 mod templates;
 
-fn main() -> std::io::Result<()> {
-    let _ = fs::create_dir("public");
-    parse_markdown_files(Path::new("md"), Path::new("public"))?;
+const PUBLIC_DIR: &str = "public";
+const MD: &str = "md";
+const HTML: &str = "html";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    parse_markdown_files(Path::new(MD), Path::new(PUBLIC_DIR))?;
+    
+    tokio::task::spawn_blocking(move || {
+       let mut hotwatch = hotwatch::Hotwatch::new().unwrap(); 
+       hotwatch
+           .watch(MD, |_| {
+               parse_markdown_files(Path::new(MD), Path::new(PUBLIC_DIR)).unwrap();
+           })
+           .expect("Failed to watch for changes in md folder");
+        loop {
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    
+    let service = tower_http::services::ServeDir::new(PUBLIC_DIR);
+    let app = Router::new().fallback_service(service);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
+    axum::serve(listener, app.into_make_service()).await?;
+
     Ok(())
+}
+
+fn generate_site() {
+
 }
 
 fn parse_markdown_files(src: &Path, dest: &Path) -> std::io::Result<()> {
     for entry in fs::read_dir(src)? {
         let entry = entry?;
         let path = entry.path();
-        let relative_path = path.strip_prefix("md").unwrap();
+        let relative_path = path.strip_prefix(MD).unwrap();
         let target_path = dest.join(relative_path);
         if path.is_dir() {
             fs::create_dir_all(&target_path)?;
             parse_markdown_files(&path, dest)?;
         } else if let Some(extension) = path.extension() {
-            if extension == "md" {
+            if extension == MD {
                 let markdown = fs::read_to_string(&path).unwrap();
                 let parser = pulldown_cmark::Parser::new_ext(&markdown, pulldown_cmark::Options::all());
 
@@ -28,14 +57,15 @@ fn parse_markdown_files(src: &Path, dest: &Path) -> std::io::Result<()> {
                 html.push_str(templates::create_body(&body).as_str());
                 html.push_str(templates::FOOTER);
 
-                let html_target = target_path.with_extension("html");
+                let html_target = target_path.with_extension(HTML);
                 fs::create_dir_all(html_target.parent().unwrap())?;
                 fs::write(html_target, html)?;
-            } else if extension == "html" {
+            } else if extension == HTML {
                 fs::create_dir_all(target_path.parent().unwrap())?;
                 fs::copy(&path, &target_path)?;
             }
         }
     }
+    generate_site();
     Ok(())
 }
