e10dccf7f5d9e0c6b4b00587487d0d8d97743033
lathan1623 <lathan1623@me.com>
2025-07-22 21:38:43 -0500

    Add md to html conversion, write to static dir

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{fs, path::Path};
+use std::{fs, path::Path, error::Error};
 
 fn main() -> std::io::Result<()> {
-    let _ = fs::create_dir("/static");
+    let _ = fs::create_dir("./static");
     build_static_dir("md");
     Ok(())
 }
@@ -17,19 +17,28 @@ fn main() -> std::io::Result<()> {
 /// the static directory.
 fn build_static_dir(dir_path: &str) {
     let destination = Path::new("./static");
-    fs::read_dir(dir_path)
-        .expect("Cannot find the md directory.")
-        .for_each(|entry| {
-            let path = entry.unwrap().path();
-            if let Some(extension) = path.extension() {
-                if extension == "md" || extension == "html" {
-                    let name = path.file_name().unwrap();
-                    //TODO convert md to html, just move html directly
-                    let _ = fs::copy(&path, &destination.join(name));
-                }
-            } else {
-                //TODO handle directories here with recursion
-
+    let dir = fs::read_dir(dir_path).expect("Cannot find the {dir_path} directory");
+    for entry in dir {
+        let path = entry.unwrap().path();
+        if let Some(extension) = path.extension() {
+            if extension == "md" {
+                let markdown = fs::read_to_string(&path).unwrap();
+                let parser = pulldown_cmark::Parser::new_ext(&markdown, pulldown_cmark::Options::all());
+                let mut body = String::new();
+                pulldown_cmark::html::push_html(&mut body, parser);
+                let mut file_name = std::path::PathBuf::from("./static");
+                file_name.push(&path.file_stem().unwrap());
+                file_name.set_extension("html");
+                let _ = fs::write(file_name, body);
             }
-        })
+            else if extension == "html" {
+                let name = path.file_name().unwrap();
+                //TODO convert md to html, just move html directly
+                let _ = fs::copy(&path, &destination.join(name));
+            }
+        } else {
+            //TODO handle directories here with recursion
+
+        }
+    }
 }
