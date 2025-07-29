54a7998d05372422f1c2d01c221672200be98a93
lathan1623 <lathan1623@me.com>
2025-07-28 21:41:38 -0500

    Add support for user to customize the hompage with index.html file

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,21 @@ use axum::Router;
 mod templates;
 
 const PUBLIC_DIR: &str = "public";
-const MD: &str = "md";
+const CONTENT: &str = "content";
 const HTML: &str = "html";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    build_html(Path::new(MD), Path::new(PUBLIC_DIR))?;
+    build_html(Path::new(CONTENT), Path::new(PUBLIC_DIR))?;
     
     tokio::task::spawn_blocking(move || {
        let mut hotwatch = hotwatch::Hotwatch::new().unwrap(); 
        hotwatch
-           .watch(MD, |_| {
-               println!("Detected change in md folder");
-               build_html(Path::new(MD), Path::new(PUBLIC_DIR)).unwrap();
+           .watch(CONTENT, |_| {
+               println!("Detected change in content folder");
+               build_html(Path::new(CONTENT), Path::new(PUBLIC_DIR)).unwrap();
            })
-           .expect("Failed to watch for changes in md folder");
+           .expect("Failed to watch for changes in content folder");
         loop {
             thread::sleep(Duration::from_secs(1));
         }
@@ -35,10 +35,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn generate_site(files: Vec<String>, dest: &Path) -> Result<(), Box<dyn Error>> {
-    let mut html = templates::HEADER.to_owned();
-    let body = files
-        .into_iter()
+fn generate_links(files: &[String], dest: &Path) -> Result<String, Box<dyn Error>> {
+    let links = files
+        .iter()
         .map(|file| {
             let file = file.trim_start_matches(dest.to_str().unwrap());
             let title = file.trim_start_matches("/").trim_end_matches(".html").trim_end_matches("index");
@@ -47,11 +46,34 @@ fn generate_site(files: Vec<String>, dest: &Path) -> Result<(), Box<dyn Error>> 
         .collect::<Vec<String>>()
         .join("<br/>\n");
     
+    Ok(links)
+}
+
+fn generate_default_index_html(html_files: &[String], dest: &Path) -> Result<String, Box<dyn Error>> {
+    let mut html = templates::HEADER.to_owned();
+    let body = generate_links(html_files, dest)?;
+    
     html.push_str(templates::create_body(&body).as_str());
     html.push_str(templates::FOOTER);
+    
+    Ok(html)
+}
 
-    let index_path = dest.join("index.html");
-    fs::write(index_path, html)?;
+fn build_index_html(src: &Path, dest: &Path, html_files: &[String]) -> Result<(), Box<dyn Error>> {
+    let index_path = src.join("index.html");
+    
+    if !index_path.exists() {
+        let default_html = generate_default_index_html(html_files, dest)?;
+        fs::write(&index_path, default_html)?;
+    }
+    
+    let custom_html = fs::read_to_string(&index_path)?;
+    let links = generate_links(html_files, dest)?;
+    let final_html = custom_html.replace("{{LINKS}}", &links);
+    
+    let dest_index_path = dest.join("index.html");
+    fs::write(dest_index_path, final_html)?;
+    
     Ok(())
 }
 
@@ -80,25 +102,31 @@ fn build_html(src: &Path, dest: &Path) -> Result<(), Box<dyn Error>> {
             generated_file.push_str("/index.html");
             html_files.push(generated_file);
         } else {
-            let extension = path.extension().context("Failed to parse extension from file path")?;
-            if extension == "css" {
-                fs::copy(&path, dest.join(path.file_name().unwrap()))?;
-                continue;
+            let extension = path.extension().context("Failed to parse extension from file path")?.to_str().unwrap();
+            match extension {
+                "css" => {
+                    fs::copy(&path, dest.join(path.file_name().unwrap()))?;
+                },
+                "html" => {
+                    continue;
+                },
+                _ => {
+                    let markdown = fs::read_to_string(&path).unwrap();
+                    let parser = pulldown_cmark::Parser::new_ext(&markdown, pulldown_cmark::Options::all());
+        
+                    let mut body = String::new();
+                    pulldown_cmark::html::push_html(&mut body, parser);
+                    html.push_str(templates::create_body(&body).as_str());
+                    html.push_str(templates::FOOTER);
+        
+                    fs::create_dir_all(html_target.parent().unwrap())?;
+                    fs::write(&html_target, &html)?;
+                    html_files.push(html_target.into_os_string().into_string().unwrap());
+                }
             }
-            let markdown = fs::read_to_string(&path).unwrap();
-            let parser = pulldown_cmark::Parser::new_ext(&markdown, pulldown_cmark::Options::all());
-
-            let mut body = String::new();
-            pulldown_cmark::html::push_html(&mut body, parser);
-            html.push_str(templates::create_body(&body).as_str());
-            html.push_str(templates::FOOTER);
-
-            fs::create_dir_all(html_target.parent().unwrap())?;
-            fs::write(&html_target, &html)?;
-            html_files.push(html_target.into_os_string().into_string().unwrap());
         }
     }
 
-    generate_site(html_files, dest)?;
+    build_index_html(src, dest, &html_files)?;
     Ok(())
 }
